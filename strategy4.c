4ad13f0ae1113fb53edc046fdd54198299552ccf
Anmol <anmol.agarwalcp810@gmail.com>
2021-04-26 19:02:50 +0530

    strategy4 name change

--- strategy4.c
+++ strategy4.c
@@ -52,40 +52,11 @@ void print_matrix_transpose(double **A,int n){
 	}
 }
 
-// void crout(double const **A, double **L, double **U, int n,int my_rank,int comm_sz) {
-// 	int i, j, k;
-// 	double sum = 0;
-
-// 	for (j=0; j<n; j++) {
-// 		// First do with scatter, then try with scatterv() later.
-// 		for (i = j; i < n; i++) {
-// 			sum = 0;
-// 			for (k = 0; k < j; k++) {
-// 				sum = sum + L[i][k] * U[k][j];
-// 			} 
-// 			L[i][j] = A[i][j] -	sum;
-// 			if(i==j){
-// 				if (L[j][j] == 0) {
-// 					exit(0);
-// 				} 
-// 			}
-// 		} 
-// 		for (i=j; i<n; i++) {
-// 			sum = 0;
-// 			for(k = 0; k < j; k++) {
-// 				sum = sum + L[j][k] * U[k][i];
-// 			} 
-// 			U[j][i] =(A[j][i] -	sum) / L[j][j];
-// 		}
-// 		MPI_Barrier(MPI_COMM_WORLD);
-// 	}
-// }
-
 int main(int argc,char* argv[]){
 
 	double start_main,end_main,start,end;
 
-	int n;
+	int n,N,num_threads;
 	int comm_sz;	// number of process
 	int my_rank;	// my process rank
 
@@ -98,12 +69,15 @@ int main(int argc,char* argv[]){
 		start_main = MPI_Wtime();
 	}
 
-	n = atoi(argv[1]);
-	
-	// int (*arr)[COLS] = malloc(sizeof *arr * ROWS);
+	N = atoi(argv[1]);
+	num_threads = atoi(argv[3]);
 
-	double initStart,intiEnd;
-	if(my_rank==0) initStart = MPI_Wtime();
+
+	// plan is to add padding so that scatter/gather works
+	int padding = N-N%num_threads;
+	if(padding==N)padding=0;
+
+	n = N + padding;
 
 	double (*A)[n] = (double(*)[n])malloc(sizeof(*A)*n);
 	double (*L)[n] = (double(*)[n])malloc(sizeof(*L)*n);
@@ -116,16 +90,21 @@ int main(int argc,char* argv[]){
 		char* input = argv[2];
 		
 		FILE* f = fopen(input,"r");
-		for(int i=0;i<n*n;i++){
-			for(int j=0;j<n;j++){
+		for(int i=0;i<N;i++){
+			for(int j=0;j<N;j++){
 				fscanf(f,"%lf",&A[i][j]);
+			}
+		}
+		for(int i=N;i<n;i++){
+			for(int j=N;j<n;j++){
+				A[i][j]=0;
 			}
 		}
 
 		for(int i=0;i<n;i++){
 			for(int j=0;j<n;j++){
 				L[i][j]=0;
-				if(i==j){
+				if(i<N && i==j){
 					U[i][j]=1;
 				}
 				else{
@@ -137,26 +116,12 @@ int main(int argc,char* argv[]){
 		// take transpose of U
 	}	
 
-
-	double bStart,bEnd;
-	if(my_rank==0)bStart = MPI_Wtime();
-
-	// if(my_rank==0){
-	// 	start = MPI_Wtime();
-	// }
-
 	MPI_Bcast(&(A[0][0]),n*n,MPI_DOUBLE,0,MPI_COMM_WORLD);
-
-	if(my_rank==0){
-		bEnd = MPI_Wtime();
-		printf("bCAST: %0.12f\n",bEnd-bStart);
-	}
-	// CROUT MATRIX DECOMPOSITION STARTS HERE
-
 
 	if(my_rank==0){
 		start = MPI_Wtime();
 	}
+	// CROUT MATRIX DECOMPOSITION STARTS HERE
 
 	int i, j, k;
 	double sum = 0;
@@ -166,66 +131,38 @@ int main(int argc,char* argv[]){
 	double (*sub_L)[n] = (double(*)[n])malloc(sizeof(*sub_L)*chunk);
 	double (*sub_U)[n] = (double(*)[n])malloc(sizeof(*sub_U)*chunk);
 	int left_i,right_i;
-
-	// Add displs, count for scatterv,gatherv
-
-	int* displs = (int*)malloc(comm_sz*sizeof(int));
-	int* rec_counts = (int*)malloc(comm_sz*sizeof(int));
-
-
 	MPI_Barrier(MPI_COMM_WORLD);
 
-	double tempStart,tempEnd;
-
-	double totalTemp=0;
-
-	for (j=0; j<n; j++) {
-
-		chunk = (n-j+1)/comm_sz;
-		displs[0]=0;
-		for(int m=0;m<comm_sz;m++){
-			rec_counts[m] = chunk;
-			if(m==comm_sz-1){
-				rec_counts[m]=(n-j+1)-(comm_sz-1)*chunk;
-			}
-			if(m>0){
-				/* different displacements for each process
-				   but we want the scattered and gathered data compressed, 
-				   hence displs[m]-displs[m-1]=rec_counts[m]
-				*/
-				displs[m]=displs[m-1]+rec_counts[m];
-			}
-		}
-
-		// reset left_i, right_i
-
-		/* Allocating iteration ranges */
-		// if(my_rank*chunk>=j){
-		// 	left_i=0;
-		// 	right_i=chunk;
-		// }
-		// else{
-		// 	if(j-my_rank*chunk<chunk){
-		// 		left_i=j-my_rank*chunk;
-		// 		right_i=chunk;
-		// 	}
-		// 	else{
-		// 		left_i=0;
-		// 		right_i=-1;
-		// 	}
-		// }
-
-		left_i = my_rank*chunk;
-		right_i = left_i+chunk;
-		if(my_rank==comm_sz-1){
-			right_i = n;
-		}
-
-		MPI_Scatterv(&(L[j][0]),rec_counts,displs,MPI_DOUBLE,sub_L,rec_counts[my_rank],MPI_DOUBLE,0,MPI_COMM_WORLD);
-
-		// MPI_Scatter(&(L[0][0]),chunk*n,MPI_DOUBLE,sub_L,chunk*n,MPI_DOUBLE,0,MPI_COMM_WORLD);
+	for (j=0; j<N; j++) {
+		// First do with scatter, then try with scatterv() later.
+		MPI_Scatter(&(L[0][0]),chunk*n,MPI_DOUBLE,sub_L,chunk*n,MPI_DOUBLE,0,MPI_COMM_WORLD);
 		MPI_Bcast(U[j],n,MPI_DOUBLE,0,MPI_COMM_WORLD);
 		MPI_Barrier(MPI_COMM_WORLD);
+
+		/* Allocating iteration ranges */
+		if(my_rank*chunk>=j){
+			left_i=0;
+			if(my_rank*chunk>=N){
+				right_i=-1;
+			}
+			else if(my_rank*chunk+chunk>=N){
+				right_i=N-my_rank*chunk;
+			}
+			else{
+				right_i=chunk;
+			}
+		}
+		else{
+			if(j-my_rank*chunk<chunk){
+				left_i=j-my_rank*chunk;
+				right_i=chunk;
+			}
+			else{
+				left_i=0;
+				right_i=-1;
+			}
+		}
+
 
 		// L's Loop
 		for (i = left_i; i < right_i; i++) {
@@ -233,26 +170,21 @@ int main(int argc,char* argv[]){
 			for (k = 0; k < j; k++) {
 				sum = sum + sub_L[i][k] * U[j][k];
 			}
-			// printf("my_rank %d, i %d,j %d,A[%d][%d]:%0.12f ,sum %0.12f\n",my_rank,i,j,my_rank*chunk+ i,j,A[my_rank*chunk+ i][j],sum);
-			sub_L[i][j]=A[j+my_rank*chunk+ i][j] - sum;
+			sub_L[i][j]=A[my_rank*chunk+ i][j] - sum;
 		}
 
-		// MPI_Gather(sub_L,chunk*n,MPI_DOUBLE,&(L[0][0]),chunk*n,MPI_DOUBLE,0,MPI_COMM_WORLD);
-		MPI_Gatherv(sub_L,rec_counts[my_rank],MPI_DOUBLE,&(L[j][0]),rec_counts,displs,MPI_DOUBLE,0,MPI_COMM_WORLD);
+		MPI_Gather(sub_L,chunk*n,MPI_DOUBLE,&(L[0][0]),chunk*n,MPI_DOUBLE,0,MPI_COMM_WORLD);
 		MPI_Barrier(MPI_COMM_WORLD);
 
 		// Checking for L[j][j]==0 once
 		if(my_rank==0){
-			// printf("L[%d][%d]: %0.12f\n",j,j,L[j][j] );
 			if (L[j][j] == 0) {
 				printf("-------------------------------------TERMINATED\n");
-				MPI_Finalize();
-				exit(0);
+				MPI_Abort(MPI_COMM_WORLD,0);
 			} 
 		}
 		
-		MPI_Scatterv(&(U[j][0]),rec_counts,displs,MPI_DOUBLE,sub_U,rec_counts[my_rank],MPI_DOUBLE,0,MPI_COMM_WORLD);
-		// MPI_Scatter(&(U[0][0]),chunk*n,MPI_DOUBLE,sub_U,chunk*n,MPI_DOUBLE,0,MPI_COMM_WORLD);
+		MPI_Scatter(&(U[0][0]),chunk*n,MPI_DOUBLE,sub_U,chunk*n,MPI_DOUBLE,0,MPI_COMM_WORLD);
 		MPI_Bcast(L[j],n,MPI_DOUBLE,0,MPI_COMM_WORLD);
 		MPI_Barrier(MPI_COMM_WORLD);
 
@@ -262,10 +194,10 @@ int main(int argc,char* argv[]){
 			for(k = 0; k < j; k++) {
 				sum = sum + L[j][k] * sub_U[i][k];
 			} 
-			sub_U[i][j] =(A[j][j+chunk*my_rank+i] - sum) / L[j][j];
+			sub_U[i][j] =(A[j][chunk*my_rank+i] - sum) / L[j][j];
 		}
-		// MPI_Gather(sub_U,chunk*n,MPI_DOUBLE,&(U[0][0]),chunk*n,MPI_DOUBLE,0,MPI_COMM_WORLD);
-		MPI_Gatherv(sub_U,rec_counts[my_rank],MPI_DOUBLE,&(U[j][0]),rec_counts,displs,MPI_DOUBLE,0,MPI_COMM_WORLD);
+		MPI_Gather(sub_U,chunk*n,MPI_DOUBLE,&(U[0][0]),chunk*n,MPI_DOUBLE,0,MPI_COMM_WORLD);
+
 		// Placed barrier at the end, so that all processes complete before going to next loop
 		MPI_Barrier(MPI_COMM_WORLD);
 	}
@@ -274,59 +206,43 @@ int main(int argc,char* argv[]){
 
 	// CROUT MATRIX DECOMPOSITION ENDS HERE
 
-	/* 
-	   Convert double[][] to double** here before calling print_matrix()
-	   scatter only works with double[][]
-	   if we try double**, then 1. not contiguous, 2. we might place burden on process 0 to scatter data.
-	*/
+	// Convert double[][] to double** here before calling print_matrix()
+	// scatter only works with double[][]
+	// if we try double**, then 1. not contiguous, 2. we might place burden on process 0 to scatter data.
 
 	if(my_rank==0){
 		end = MPI_Wtime();
 		printf("strategy4 CROUT: %0.12f\n",end-start);
-		printf("Total Accumulation: %0.12f\n",totalTemp );
 	}
 
 	if(my_rank==0){
 
-		double* A1[n],*L1[n],*U1[n];
-		for(i=0;i<n;i++){
-			A1[i] = (double*)malloc(sizeof(double)*n);
-			L1[i] = (double*)malloc(sizeof(double)*n);
-			U1[i] = (double*)malloc(sizeof(double)*n);
+		double* A1[N],*L1[N],*U1[N];
+		for(i=0;i<N;i++){
+			A1[i] = (double*)malloc(sizeof(double)*N);
+			L1[i] = (double*)malloc(sizeof(double)*N);
+			U1[i] = (double*)malloc(sizeof(double)*N);
 		}
 
-		for(i=0;i<n;i++){
-			for(j=0;j<n;j++){
+		for(i=0;i<N;i++){
+			for(j=0;j<N;j++){
 				A1[i][j]=A[i][j];
 				L1[i][j]=L[i][j];
 				U1[i][j]=U[j][i];
 			}
 		}
 
-		// printf("A\n");
-		// now check
-		// print_matrix((double**)A,n);
-
-		// printf("------------------------------\n");
-		// printf("L\n");
-		// output_(L/U)_<strategy(0/1/2/3/4)>_<number of threads/processes(2/4/8/16)>.txt
-		// print_matrix((double**)L,n);
 
 		char str[100] = "output_L_4_";
 		char intbuf[10];
 		sprintf(intbuf,"%d",comm_sz);
 		strcat(str,intbuf);
 		strcat(str,".txt");
-		// printf("247\n");
-		write_output(str,L1,n);
-		// printf("249\n");
-		// printf("------------------------------\n");
-		// printf("U\n");
-		// print_matrix(U1,n);
+		write_output(str,L1,N);
 		char str2[100] = "output_U_4_";
 		strcat(str2,intbuf);
 		strcat(str2,".txt");
-		write_output(str2,U1,n);
+		write_output(str2,U1,N);
 
 	}
 	  
